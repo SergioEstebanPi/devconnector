feaaa4e1da6d12af05bfac2aace8450f3c629c70
mac <mac@macs-MacBook-Pro>
2020-01-13 23:13:14 -0500

    Autenticacion por token y devolucion del usuario que inicia la sesion

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-
+const User = require('../../models/User');
 
 // @route GET api/auths
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            msg: 'Server error'
+        });
+    }
+});
 
 module.exports = router;
