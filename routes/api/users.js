3857cf93285c6f75c8c96e14ef605cd7177d9252
mac <mac@macs-MacBook-Pro>
2020-01-13 22:44:16 -0500

    Agrego el registro de los usuarios

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {
     check,
     validationResult
 } = require('express-validator/check');
 
+
+const User = require('../../models/User');
 
 // @route GET api/users
 // @desc Test route
@@ -21,7 +25,7 @@ router.post('/', [
             min: 6
         })
     ],
-    (req, res) => {
+    async (req, res) => {
         //console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -29,7 +33,58 @@ router.post('/', [
                 errors: errors.array()
             });
         }
-        res.send('User route');
+
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+
+        try {
+            let user = await User.findOne({
+                email
+            });
+
+            // See if user exists
+            if (user) {
+                res.status(400).json({
+                    errors: [{
+                        msg: 'User already exists'
+                    }]
+                });
+            }
+
+            // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200', // size
+                r: 'pg', // reading
+                d: 'mm', // has no avatar
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            // Encrypt password
+            const salt = await bcrypt.genSalt(10); // hash 10 (recomended in doc)
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // Return jsonwebtoken
+
+            res.send('User register');
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+
+        //res.send('User route');
     }
 );
 
