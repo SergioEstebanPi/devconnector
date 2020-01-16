e0482ba5dd7e93dee7c53551ff36bfcb66e68d2a
mac <mac@macs-MacBook-Pro>
2020-01-15 20:56:56 -0500

    Agrego ruta para eliminar el perfil y el usuario con el token

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -172,4 +172,31 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user and posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+
+    try {
+        // @todo - remove users posts
+
+        // Remove profile
+        const profile = await Profile.findOneAndRemove({
+            user: req.user.id
+        });
+
+        // Remove user
+        const user = await User.findOneAndRemove({
+            _id: req.user.id
+        });
+
+        res.json({
+            msg: 'User deleted'
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
