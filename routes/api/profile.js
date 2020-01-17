75acd5312401be0479c81f264f541629b64cd9e3
mac <mac@macs-MacBook-Pro>
2020-01-16 20:04:51 -0500

    Agrego ruta de borrado de experiencia segun el id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -257,4 +257,57 @@ router.put('/experience', [auth, [
     }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user and posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+
+    try {
+        // @todo - remove users posts
+
+        // Remove profile
+        const profile = await Profile.findOneAndRemove({
+            user: req.user.id
+        });
+
+        // Remove user
+        const user = await User.findOneAndRemove({
+            _id: req.user.id
+        });
+
+        res.json({
+            msg: 'User deleted'
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete profile experience
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        });
+
+        // Get remove index
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
