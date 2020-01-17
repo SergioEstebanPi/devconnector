27996112e90aefa30afc261c5fe438093a677d3f
mac <mac@macs-MacBook-Pro>
2020-01-16 20:17:33 -0500

    Agrego creacion y borrado de educacion dentro del perfil

--- models/Profile.js
+++ models/Profile.js
@@ -64,7 +64,7 @@ const ProfileSchema = new mongoose.Schema({
             type: String,
             required: true
         },
-        fieldofstydy: {
+        fieldofstudy: {
             type: String,
             required: true
         },
