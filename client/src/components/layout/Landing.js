9e446350e87b27d7dfe182df9d36955f8e101663
mac <mac@macs-MacBook-Pro>
2020-01-17 23:33:52 -0500

    Configuracion de las ruta de React

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
     return (
@@ -11,8 +12,10 @@ const Landing = () => {
                 other developers
                 </p>
                 <div className="buttons">
-                <a href="register.html" className="btn btn-primary">Sign Up</a>
-                <a href="login.html" className="btn btn-light">Login</a>
+                {/* <a href="register.html" className="btn btn-primary">Sign Up</a> */}
+                <Link to="/register" className="btn btn-primary">Sign Up</Link>
+                {/* <a href="login.html" className="btn btn-light">Login</a> */}
+                <Link to="/login" className="btn btn-light">Login</Link>
                 </div>
             </div>
             </div>
