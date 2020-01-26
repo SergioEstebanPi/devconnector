75b962e43c2ada08138ea49e599385384d542e3c
mac <mac@macs-MacBook-Pro>
2020-01-26 07:34:27 -0500

    Agrego componente para mostrar los repositorios del perfil

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ 
     getProfileById,
@@ -85,45 +86,14 @@ const Profile = ({
                     </div>
 
                     {/* <!-- Github --> */}
-                    <div className="profile-github">
-                        <h2 className="text-primary my-1">
-                        <i className="fab fa-github"></i> Github Repos
-                        </h2>
-                        <div className="repo bg-white p-1 my-1">
-                        <div>
-                            <h4><a href="#!" target="_blank"
-                                rel="noopener noreferrer">Repo One</a></h4>
-                            <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Repellat, laborum!
-                            </p>
-                        </div>
-                        <div>
-                            <ul>
-                            <li className="badge badge-primary">Stars: 44</li>
-                            <li className="badge badge-dark">Watchers: 21</li>
-                            <li className="badge badge-light">Forks: 25</li>
-                            </ul>
-                        </div>
-                        </div>
-                        <div className="repo bg-white p-1 my-1">
-                        <div>
-                            <h4><a href="#!" target="_blank"
-                                rel="noopener noreferrer">Repo Two</a></h4>
-                            <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Repellat, laborum!
-                            </p>
-                        </div>
-                        <div>
-                            <ul>
-                            <li className="badge badge-primary">Stars: 44</li>
-                            <li className="badge badge-dark">Watchers: 21</li>
-                            <li className="badge badge-light">Forks: 25</li>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
+                    {
+                        profile.githubusername && (
+                            <Fragment>
+                                <ProfileGithub username={profile.githubusername} />
+                            </Fragment>
+                        )
+                    }
+
                 </div>
 
                 </Fragment>
