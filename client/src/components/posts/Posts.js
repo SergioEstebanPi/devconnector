83966b14e386d04792472bd39407ddf711e8abc4
mac <mac@macs-MacBook-Pro>
2020-02-06 22:42:05 -0500

    Adding posts and link to the publisher user

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
+import PostForm from './PostForm';
 
 const Posts = ({
     getPosts,
@@ -19,24 +20,9 @@ const Posts = ({
             </h1>
             <p className="lead"><i className="fas fa-user"></i> Welcome to the community!</p>
 
-            <div className="post-form">
-                <div className="bg-primary p">
-                <h3>Say Something...</h3>
-                </div>
-                <form className="form my-1">
-                <textarea
-                    name="text"
-                    cols="30"
-                    rows="5"
-                    placeholder="Create a post"
-                    required
-                ></textarea>
-                <input type="submit" className="btn btn-dark my-1" value="Submit" />
-                </form>
-            </div>
-
-
             {/* PostForm */}
+            <PostForm />
+
             <div className="posts">
                 {
                     posts.map(post => (
