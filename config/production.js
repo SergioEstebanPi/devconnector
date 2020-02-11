ff0aabd2a4891e13654a2f822054d6cd5e2822f0
mac <mac@macs-MacBook-Pro>
2020-02-11 00:31:41 -0500

    Ajusto configuracion de ambientes

---
+++ config/production.js
@@ -0,0 +1,6 @@
+{
+    "mongoURI": "mongodb+srv://devconndbuser:<devconndbuser>@devconndb-b8plu.mongodb.net/test?retryWrites=true&w=majority",
+    "jwtSecret": "123456",
+    "githubClientId": "githubClientId",
+    "githubSecret": "githubSecret"
+}
